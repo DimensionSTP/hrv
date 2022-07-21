b22444763d50c641a0f4229e2d4d9533c2eaf066
DimensionSTP <ddang8jh@gmail.com>
2022-07-21 20:29:16 +0900

    skiprows to read raw data

--- src/main.py
+++ src/main.py
@@ -14,65 +14,78 @@ FILE_NAME = os.listdir(DATA_PATH)
 SAVE_PATH = "./result"
 hrv = HRV()
 
+
 def main(
-    window_size: int, 
+    window_size: int,
     interval_size: int,
-    sampling_rate: int, 
-    data_path: str, 
-    file_name: List, 
-    save_path: str
+    sampling_rate: int,
+    data_path: str,
+    file_name: List,
+    save_path: str,
 ):
     for file in file_name:
-        ppg_df = pd.read_csv(f"{data_path}/{file}", header=None, delimiter="\t")
+        ppg_df = pd.read_csv(
+            f"{data_path}/{file}", header=None, delimiter="\t", skiprows=23
+        )
         print(file + "분석 시작!")
-        
+
         ppg_value = ppg_df[1].values
         # Sliding window
         hrv_list = []
-        
-        for i in range(0, len(ppg_value) - window_size * sampling_rate, interval_size * sampling_rate):
-            sliding_ppg = ppg_value[i:i + window_size * sampling_rate]
+
+        for i in range(
+            0,
+            len(ppg_value) - window_size * sampling_rate,
+            interval_size * sampling_rate,
+        ):
+            sliding_ppg = ppg_value[i : i + window_size * sampling_rate]
             sliding_ppg = sliding_ppg.astype("float")
             # Peak detection
-            peak = mpd.detect_peaks(sliding_ppg, fs=sampling_rate, detrend_factor=1, is_display=False)
+            peak = mpd.detect_peaks(
+                sliding_ppg, fs=sampling_rate, detrend_factor=1, is_display=False
+            )
             # Calcualte HRV
             hrv_list.append(hrv(peak=peak, sampling_rate=sampling_rate))
         # file write (.csv)
         df = pd.DataFrame(hrv_list)
         df.columns = [
-            "RRI", 
-            "BPM", 
-            "SDNN", 
-            "rMSSD", 
-            "pNN50", 
-            "VLF", 
-            "LF", 
-            "HF", 
-            "VLFp", 
-            "LFp", 
-            "HFp", 
-            "lnVLF", 
-            "lnLF", 
-            "lnHF", 
-            "VLF/HF", 
-            "LF/HF", 
-            "tPow", 
-            "dPow", 
-            "dHz", 
-            "pPow", 
-            "pHz", 
-            "CohRatio", 
-            "RSA_PB"
+            "RRI",
+            "BPM",
+            "SDNN",
+            "rMSSD",
+            "pNN50",
+            "VLF",
+            "LF",
+            "HF",
+            "VLFp",
+            "LFp",
+            "HFp",
+            "lnVLF",
+            "lnLF",
+            "lnHF",
+            "VLF/HF",
+            "LF/HF",
+            "tPow",
+            "dPow",
+            "dHz",
+            "pPow",
+            "pHz",
+            "CohRatio",
+            "RSA_PB",
         ]
-        df.to_csv(f"{save_path}/{file[:-4]}_{window_size}.csv", encoding="utf-8-sig", index=False) #결과 저장
-        
-        
+        df.to_csv(
+            f"{save_path}/{file[:-4]}_{window_size}.csv",
+            encoding="utf-8-sig",
+            index=False,
+        )  # 결과 저장
+
+
 if __name__ == "__main__":
     main(
-        window_size=WINDOW_SIZE, 
-        interval_size=INTERVAL_SIZE, 
+        window_size=WINDOW_SIZE,
+        interval_size=INTERVAL_SIZE,
         sampling_rate=SAMPLING_RATE,
-        data_path = DATA_PATH,  
-        file_name=FILE_NAME, 
-        save_path = SAVE_PATH
+        data_path=DATA_PATH,
+        file_name=FILE_NAME,
+        save_path=SAVE_PATH,
     )
